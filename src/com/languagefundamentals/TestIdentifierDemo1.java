7ef4fc06fe190ce44ae3200aec54a77bf9c6e91f
neeraja <neerajaalavala@gmail.com>
2026-04-29 08:23:00 +0530

    Identifiers 1

---
+++ src/com/languagefundamentals/TestIdentifierDemo1.java
@@ -0,0 +1,34 @@
+package com.languagefundamentals;
+
+public class TestIdentifierDemo1 {
+	
+	int stu_id;
+	String stu_name;
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		
+		TestIdentifierDemo1 t1=new TestIdentifierDemo1();
+		System.out.println("t1.stu_id");
+		System.out.println("t1.stu_id");
+		
+		t1.method$1();
+	}
+		
+		
+		void method$1() {
+			
+			
+			System.out.println("method1 called");
+			
+		
+		}
+		
+
+	}
+
+
