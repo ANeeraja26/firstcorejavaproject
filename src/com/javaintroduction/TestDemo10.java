eb2dbf67b8d8dbd1c17494798d603809c89ccff3
neeraja <neerajaalavala@gmail.com>
2026-04-30 08:59:31 +0530

    object Create

---
+++ src/com/javaintroduction/TestDemo10.java
@@ -0,0 +1,35 @@
+package com.javaintroduction;
+
+public class TestDemo10 {
+	
+	static int objectcount =0;
+	
+	    {
+		 
+	       objectcount ++;
+
+		 }
+	
+
+	public static void main(String[] args) {
+        
+		
+		
+		TestDemo10 A =new TestDemo10();
+		TestDemo10 B =new TestDemo10();
+		TestDemo10 C =new TestDemo10();
+		TestDemo10 D =new TestDemo10();
+		
+		
+		System.out.println(A.objectcount);
+		
+		
+		
+		
+		
+	}
+
+}
+ 
+    
+    
